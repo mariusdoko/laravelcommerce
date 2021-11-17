3d3da95a7705b562e8c4bfa466360bb86322d6a0
Marius Doko <marius_doko@outlook.com>
2021-11-17 13:29:09 +0100

    User profile, Update and change password

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -20,24 +20,25 @@
                                 @csrf
                                 <div class="row">
                                     <div class="col-12">
-<div class="row">
-    <div class="col-md-6">
-        <div class="form-group">
-            <h5>Admin User Name <span class="text-danger">*</span></h5>
-            <div class="controls">
-                <input type="text" name="name" class="form-control" required="" value="{{ $editData->name }}" > </div>
-        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <h5>Admin User Name <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="name" class="form-control" required="" value="{{ $editData->name }}" > </div>
+                                                </div>
 
-    </div> <!-- end col md 6 -->
-    <div class="col-md-6">
-        <div class="form-group">
-            <h5>Admin Email <span class="text-danger">*</span></h5>
-            <div class="controls">
-                <input type="email" name="email" class="form-control" required="" value="{{ $editData->email }}"> </div>
-        </div>
+                                            </div> <!-- end col md 6 -->
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <h5>Admin Email <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="email" name="email" class="form-control" required="" value="{{ $editData->email }}"> </div>
+                                                </div>
 
-    </div> <!-- end col md 6 -->
-</div> <!--end row-->
+                                            </div> <!-- end col md 6 -->
+                                        </div> <!--end row-->
+
                                         <div class="row">
                                             <div class="col-md-6">
                                                 <div class="form-group">
@@ -48,9 +49,10 @@
                                             </div> <!-- end col md 6 -->
 
                                             <div class="col-md-6">
+
                                                 <img id="showImage" src="{{ (!empty($editData->profile_photo_path))?
-url('upload/admin_images/'.$editData->profile_photo_path):url('upload/
-no_image.jpg') }}" style="width: 100px; height: 100px;">
+                                                 url('upload/admin_images/'.$editData->profile_photo_path):url('upload/
+                                                 no_image.jpg') }}" style="width: 100px; height: 100px;">
 
                                             </div> <!-- end col md 6 -->
 
@@ -59,9 +61,9 @@ no_image.jpg') }}" style="width: 100px; height: 100px;">
 
 
 
-                                <div class="text-xs-right">
-                                    <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update">
-                                </div>
+                                        <div class="text-xs-right">
+                                            <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update">
+                                        </div>
                             </form>
 
                         </div>
@@ -83,14 +85,13 @@ no_image.jpg') }}" style="width: 100px; height: 100px;">
 
     <script type="text/javascript">
         $(document).ready(function(){
-           $('#image').change(function(e){
-               var reader = new FileReader();
-               reader.onload = function(e){
-                   $('#showImage').attr('src',e.target.result);
-               }
-               reader.readAsDataURL(e.target.files['0']);
+            $('#image').change(function(e){
+                var reader = new FileReader();
+                reader.onload = function(e){
+                    $('#showImage').attr('src',e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
             });
         });
-
     </script>
 @endsection
